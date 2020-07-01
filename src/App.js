168434fb106c99a635b39420b2e165d56232193d
themikaelson <34260590+themikaelson@users.noreply.github.com>
2020-07-01 16:56:28 -0400

    reorganize update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 const App = () => {
@@ -74,36 +73,10 @@ const App = () => {
         <div className="container">
           <Alert alert={alert} clearAlert={clearAlert} />
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
+            <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
-            <Route
-              exact
-              path="/user/:login"
-              render={(props) => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )}
-            />
+            <Route exact path="/user/:login" component={User} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </div>
